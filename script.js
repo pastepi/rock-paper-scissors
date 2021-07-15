4597d961312237d5e0f82ca2e09f941d29eace97
pastepi <pawel.st123@gmail.com>
2021-07-15 23:34:18 +0200

    Final commit

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const buttonContainer = document.querySelector('.button-container');
 const btnImg = document.querySelectorAll('.btn-img');
 const divScore = document.querySelector('#game-score');
 const scoreHeading = document.createElement('h2');
-
+const divArena = document.createElement('div');
+const imgArenaLeft = document.createElement('img');
+const imgArenaRight = document.createElement('img');
+imgArenaLeft.classList.add('arenaImg');
+imgArenaLeft.classList.add('leftImg');
+imgArenaRight.classList.add('arenaImg');
 
 let playerWinCount = 0;
 let playerLoseCount = 0;
@@ -43,19 +48,41 @@ let playRound = (playerSelection, computerSelection) => {
 };
 
 let playGame = (playerMove) => {
-
+    let arenaImgFlag = false;
     let computerSelection = computerPlay();
+
+
+    if (!arenaImgFlag) {
+
+        divArena.appendChild(imgArenaLeft);
+        divArena.appendChild(imgArenaRight);
+        arenaImgFlag = true;
+    }
 
     switch(playRound(playerMove, computerSelection)) {
         case 0:
             console.log(`Draw! You both picked ${playerMove}.`);
+            imgArenaLeft.setAttribute('src', `static/${playerMove}1.png`);
+            imgArenaLeft.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(150deg) saturate(300%)";
+            imgArenaRight.setAttribute('src', `static/${computerSelection}1.png`);
+            imgArenaRight.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(150deg) saturate(300%)";
+
             break;
         case 1:
+            imgArenaLeft.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(60deg) saturate(300%)";
+            imgArenaLeft.setAttribute('src', `static/${playerMove}1.png`);
+            imgArenaRight.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(300deg) saturate(300%)";
+            imgArenaRight.setAttribute('src', `static/${computerSelection}1.png`)
+
             console.log(`You win! ${playerMove[0].toUpperCase() + playerMove.slice(1)} beats ${computerSelection}.`);
             playerWinCount += 1;
             break;
         case 2:
             console.log(`You lose! ${playerMove[0].toUpperCase() + playerMove.slice(1)} loses to ${computerSelection}.`);
+            imgArenaLeft.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(300deg) saturate(300%)";
+            imgArenaLeft.setAttribute('src', `static/${playerMove}1.png`);
+            imgArenaRight.style.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(60deg) saturate(300%)";
+            imgArenaRight.setAttribute('src', `static/${computerSelection}1.png`)
             playerLoseCount += 1;
             break;
         default:
@@ -77,7 +104,25 @@ buttonStart.addEventListener('click', () => {
     btnImg.forEach((img) => {
         img.classList.add('gameactive');
     })
+
     
+
+    divArena.style.cssText = 'width: 300px; height: 150px; border: 2px solid black';
+
+    divArena.animate([
+        { // from
+          opacity: 0,
+          easing: 'ease-out',
+          color: "#fff"
+        },
+        { // to
+          opacity: 1,
+          easing: 'ease-in',
+          color: "#000"
+        }
+      ], 600);
+    divScore.appendChild(divArena);
+
     scoreHeading.textContent = "Current Win-Lose: 0-0";
     scoreHeading.animate([
     { // from
@@ -100,6 +145,7 @@ buttonStart.addEventListener('click', () => {
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         scoreHeading.textContent = playGame(button.value);
+
         if (gameCounter === 5) {
             buttons.forEach((button) => {
                 button.disabled = true;
@@ -128,6 +174,9 @@ buttons.forEach((button) => {
                 }
               ], 600);
             divScore.appendChild(gameOver);
+            if (playerWinCount > playerLoseCount) {
+                document.querySelector('html').style.backgroundImage = "url('static/neverlost.png')"
+            }
         }
     })
 });
